f21993597ff1202abf65edb2c0a51403184ee052
Whonceuponatime <63453754+Whonceuponatime@users.noreply.github.com>
2021-11-22 18:51:39 +0900

    Add files via upload

---
+++ RSA Cryptosystem.py
@@ -0,0 +1,43 @@
+#RSA Cryptosystem
+print("P")
+p = input()
+print("Q")
+q = input()
+n = int(p)*int(q)
+
+def gcd(p,q):
+# Create the gcd of two positive integers.
+    while q != 0:
+        p, q = q, p%q
+    return p
+
+def is_coprime(x, y):
+    return gcd(x, y) == 1
+
+def phi_func(x):
+    if x == 1:
+        return 1
+    else:
+        n = [y for y in range(1,x) if is_coprime(x,y)]
+        return len(n)
+phi_n = (phi_func(n))
+print("Phi")
+print(phi_n)
+print("Public Key")
+public_key = int(input())
+print("Private Key")
+private_key = int(input())
+print("Message")
+message = int(input())
+def enc(public, m):
+    c = (m**public)%n
+    print("Encrypted")
+    print(c)
+    return c
+def dec(private, c):
+    m = (c**private)%n
+    print("Decrypted")
+    print(m)
+encrypted = enc(public_key, message)
+
+dec(private_key, encrypted)
